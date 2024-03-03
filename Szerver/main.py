cf27ad2e832ca5a4da869a5b98f6f6b0e54ea70b
LandiMarton <161331360+LandiMarton@users.noreply.github.com>
2024-03-03 16:24:15 +0100

    flask setup

--- Szerver/main.py
+++ Szerver/main.py
@@ -1,2 +1,9 @@
+from flask import Flask, session, request
+
+app = Flask(__name__)
+app.secret_key = "40e2d93e1f4e61d32e7c4c73de53648b811a6c0d0aa3e73ab16158aef69e43d4"
+
+
+
 if __name__ == "__main__":
-    pass
+    app.run()
