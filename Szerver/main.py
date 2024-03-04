2eac68e7160fdaee2f673b3f3236f699ec5337ef
LandiMarton <161331360+LandiMarton@users.noreply.github.com>
2024-03-04 20:38:11 +0100

    main váz

--- Szerver/main.py
+++ Szerver/main.py
@@ -6,15 +6,30 @@ from user import Level
 app = Flask(__name__)
 app.secret_key = "40e2d93e1f4e61d32e7c4c73de53648b811a6c0d0aa3e73ab16158aef69e43d4"
 
+@app.route("/login")
+def login():
+    pass
 
+@app.route("/logout")
+def logout():
+    pass
+
+@app.route("/parts-get")
+def parts_get():
+    pass
+
+@app.route("/parts-modify")
+def parts_mod():
+    pass
+
+@app.route("/users-new")
+def users_new():
+    pass
+
+@app.route("/users-modify")
+def users_modify():
+    pass
 
 if __name__ == "__main__":
     app.run()
 
-# routes:
-# login
-# logout
-# parts-modify
-# parts-get
-# users-new
-# users-modify
