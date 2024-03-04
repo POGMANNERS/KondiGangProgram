0e590cfe6860c43f94bb4a52ffbfb42caf4014f2
LandiMarton <161331360+LandiMarton@users.noreply.github.com>
2024-03-04 17:21:09 +0100

    db formázás, flask secret key és todo list

--- Szerver/adatbazis.py
+++ Szerver/adatbazis.py
@@ -1,5 +1,6 @@
 import pymongo
 from bson.objectid import ObjectId
+
 class db:
     client = pymongo.MongoClient("mongodb://localhost:27017/")
 
@@ -14,30 +15,33 @@ class db:
 
     @classmethod
     def getUser(cls, id):
-        user=cls.felhasznalok.find_one({"_id": ObjectId(id)})
+        user = cls.felhasznalok.find_one({"_id": ObjectId(id)})
         return user
+
     @classmethod
     def addPart(cls, name, price, maxnum):
-        part=cls.alkatresz.insert_one({ "alkatresz_nev": name, "alkatresz_ar":price, "maxdb": maxnum})
+        part = cls.alkatresz.insert_one({ "alkatresz_nev": name, "alkatresz_ar":price, "maxdb": maxnum})
         return part
+
     @classmethod
     def modifyPart(cls, id, name, price, maxnum):
-        part=cls.alkatresz.update_one({"_id":ObjectId(id)},{"$set":{ "alkatresz_nev": name, "alkatresz_ar":price, "maxdb": maxnum}})
+        part = cls.alkatresz.update_one({"_id":ObjectId(id)},{"$set":{ "alkatresz_nev": name, "alkatresz_ar":price, "maxdb": maxnum}})
         return part
 
     @classmethod
     def listParts(cls):
-        parts=cls.alkatresz.find()
+        parts = cls.alkatresz.find()
         return parts
 
     @classmethod
     def addUser(cls,username,password,salt,level,name):
-        user=cls.felhasznalok.insert_one({"felhasznalonev": username,"jelszo": password,"salt": salt,"jogosultsag": level,"nev": name})
+        user = cls.felhasznalok.insert_one({"felhasznalonev": username,"jelszo": password,"salt": salt,"jogosultsag": level,"nev": name})
         return user
+
     @classmethod
     def addStorage(cls):
         cls.raktar.drop()
-        storage=cls.raktar.insert_one({
+        storage = cls.raktar.insert_one({
             "sorok": [
                 {"oszlopok":  [
                     {"rekeszek":  [
@@ -52,8 +56,8 @@ class db:
 
 # ha kódot futtatsz teszteléshez, egy ilyen blokkba kell rakni
 if __name__ == "__main__":
-    #print(db.addPart("Napelem", "300", 3))
-    print(db.modifyPart("65e5aa117a834ed19b9f330b",name="napelem",price="200",maxnum=4))
+    print(db.addPart("Napelem", "300", 3))
+    print(db.modifyPart("65e5b086c099210a07e0a5e5",name="napelem",price="200",maxnum=4))
     print(*db.listParts())
     print(db.addUser("Potter","aa","bb",1,"Jandovics Ákos Attila"))
     print(db.addStorage())
--- Szerver/main.py
+++ Szerver/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, session, request
+import auth
 
 app = Flask(__name__)
 app.secret_key = "40e2d93e1f4e61d32e7c4c73de53648b811a6c0d0aa3e73ab16158aef69e43d4"
@@ -7,3 +8,11 @@ app.secret_key = "40e2d93e1f4e61d32e7c4c73de53648b811a6c0d0aa3e73ab16158aef69e43
 
 if __name__ == "__main__":
     app.run()
+
+# routes:
+# login
+# logout
+# parts-modify
+# parts-get
+# users-new
+# users-modify
